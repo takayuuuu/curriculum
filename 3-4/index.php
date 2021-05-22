ac86c648faa4edc010fd7fea87823b82148a3684
takayuuuu <yuta@takahashiyuutanoMacBook-Air.local>
2021-05-22 23:35:07 +0900

    Update index.php

--- 3-4/index.php
+++ 3-4/index.php
@@ -13,6 +13,20 @@ $getData = new getData();
 $users_data = $getData->getUserData();
 // 記事情報インスタンス化して取得
 $post_data = $getData->getPostData();
+
+// category_noについて「1だったら食事、2だったら旅行、それ以外であればその他 」と表示
+// $stmt = $pdo->query("SELECT category_no FROM posts");
+// $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+// foreach($rows as $row) {
+// if($row["category_no"]==1){
+//   echo "食事";
+// }elseif($row["category_no"]==2){
+//   echo "旅行";
+// }else{
+//   echo "その他";
+// }
+// }
+
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +36,7 @@ $post_data = $getData->getPostData();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <title>Document</title>
+  <title>３章チェックテスト</title>
 </head>
 <body>
   <header>
@@ -30,12 +44,12 @@ $post_data = $getData->getPostData();
     <div class="photo">
       <img src="./images/yigroup.png" alt="YIGroup">
     </div>
-    <div class="haeader1">
+    <div class="header1">
       <p>ようこそ<?php echo $users_data["last_name"]; ?><?php echo $users_data["first_name"]; ?>さん</p>
     </div>
     <div class="header2">  
       <p>最終日ログイン：<?php echo $users_data["last_login"]; ?></p>
-    <div>
+    </div>
     </div>
   </header>
   <main>
@@ -52,7 +66,15 @@ $post_data = $getData->getPostData();
         <tr>
          <td> <?php echo $row['id'] ?> </td>
          <td> <?php echo$row['title'] ?> </td>
-         <td> <?php echo$row['category_no'] ?> </td>
+         <td> 
+           <?php if($row["category_no"]==1){ ?>
+           <?php echo "食事" ?>
+           <?php }elseif($row["category_no"]==2){ ?>
+           <?php echo "旅行" ?>
+           <?php }else{ ?>
+           <?php echo "その他" ?>
+           <?php } ?>
+         </td>
          <td> <?php echo $row['comment'] ?> </td>
          <td> <?php echo $row['created'] ?> </td>
         </tr>
@@ -60,7 +82,7 @@ $post_data = $getData->getPostData();
       </table>
   </main>
   <footer>
-    <p>Y&I group.inc</p>
+    Y&I group.inc
   </footer>
 </body>
 </html>

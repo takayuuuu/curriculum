3d3b3d5c8f9d7637e8545476ae778d1b97628e3a
takayuuuu <yuta@takahashiyuutanoMacBook-Air.local>
2021-09-13 20:45:21 +0900

    1-3

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,28 @@
+let a = 5;
+
+if (a !== 10) {
+  console.log(a);
+}
+
+let b = 14;
+
+if (b >=10 && b < 20 && b % 2 === 0) {
+  console.log(b);
+}
+
+let x = 10;
+
+if (x >=10 && x <=20) {
+  console.log('成功です');
+} else {
+  console.log('失敗です');
+}
+
+let y = 0;
+
+if (y % 2 === 0) {
+  console.log('偶数です');
+} else if (y % 2 !== 0) {
+  console.log('奇数です');
+} 
+
